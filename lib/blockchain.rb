599eada5df71d5a43abe8f9a65a606c1afd5c628
majdeddine <majda-jambouri@hotmail.fr>
2017-12-19 16:09:56 +0000

    add a block to the blockchain

--- lib/blockchain.rb
+++ lib/blockchain.rb
@@ -9,7 +9,21 @@ class BlockChain
   end
 
   def create_genesis_block
-    return Block.new('')
+    block = Block.new('')
+    block.calculate_hash
+    return block
   end
-  
+
+  def last_block
+    return @chain.last
+  end
+
+  def add_block(transaction)
+    block = Block.new(transaction)
+    block.index = @chain.size
+    block.previous_hash = @chain.last.hash
+    block.calculate_hash
+    @chain << block
+  end
+
 end
