e2f09d919652dccf498031d85df26b6b9a30ebf7
Robert Clayton <robbie.c.clayton@gmail.com>
2017-12-21 13:21:23 +0000

    chat app complete
    to test run <node server> and visit localhost:3000 you can then visit 3000 from any number of tabs and chat through the socket connection

--- server.js
+++ server.js
@@ -19,12 +19,27 @@ io.sockets.on('connection', function(socket){
 
   // Disconnect
   socket.on('disconnect', function(data){
+    // if(!socket.user) return;
+    users.splice(users.indexOf(socket.username), 1);
+    updateUserNames();
     connections.splice(connections.indexOf(socket), 1);
     console.log('Disconnected: %s sockets connected', connections.length );
   });
 
   // Send message
   socket.on('send message', function(data){
-    io.sockets.emit('new message', {msg: data});
+    io.sockets.emit('new message', {msg: data, user: socket.username});
   });
+
+  // New Users
+  socket.on('new user', function(data, callback){
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUserNames();
+  });
+
+  function updateUserNames(){
+    io.sockets.emit('get users', users);
+  }
 });
