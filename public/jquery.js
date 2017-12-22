c7de75a838fd4844ccbb00bffeedf4757daa07ac
majdeddine <majda-jambouri@hotmail.fr>
2017-12-22 11:14:19 +0000

    add auto scroll when mining

--- public/jquery.js
+++
@@ -1,7 +0,0 @@
-// if (window.testRunner)
-//   testRunner.overridePreference('WebKitAllowRunningInsecureContent', true);
-
-$(document).ready(function(){
-    console.log('here im in SIDEDEDEDEDEDEDEDEDEDEDED');
-    $('#inside').animate({ scrollTop: $('#inside')[0].scrollHeight}, 200);
-});
